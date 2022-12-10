ae11da63eaa4e9a6d69940325f24675f26cde906
Les Aker <me@lesaker.org>
2022-12-10 16:32:31 -0500

    add xorgproto

---
+++ Formula/xorgproto.rb
@@ -0,0 +1,33 @@
+class Xorgproto < Formula
+  desc "X.Org: Protocol Headers"
+  homepage "https://www.x.org/"
+  url "https://xorg.freedesktop.org/archive/individual/proto/xorgproto-2022.2.tar.gz"
+  sha256 "da351a403d07a7006d7bdc8dcfc14ddc1b588b38fb81adab9989a8eef605757b"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    regex(/href=.*?xorgproto[._-]v?(\d+\.\d+(?:\.([0-8]\d*?)?\d(?:\.\d+)*)?)\.t/i)
+  end
+
+  depends_on "pkg-config" => [:build, :test]
+  depends_on "util-macros" => :build
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --sysconfdir=#{etc}
+      --localstatedir=#{var}
+      --disable-dependency-tracking
+      --disable-silent-rules
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    assert_equal "-I#{include}", shell_output("pkg-config --cflags xproto").chomp
+    assert_equal "-I#{include}/X11/dri", shell_output("pkg-config --cflags xf86driproto").chomp
+  end
+end
