3a9fae9bbf1e00332bce50e1b6c5cb064dd8dad0
Les Aker <me@lesaker.org>
2022-12-11 12:07:32 -0500

    add libidn

---
+++ Formula/libidn.rb
@@ -0,0 +1,23 @@
+class Libidn < Formula
+  desc "International domain name library"
+  homepage "https://www.gnu.org/software/libidn/"
+  url "https://ftp.gnu.org/gnu/libidn/libidn-1.41.tar.gz"
+  mirror "https://ftpmirror.gnu.org/libidn/libidn-1.41.tar.gz"
+  sha256 "884d706364b81abdd17bee9686d8ff2ae7431c5a14651047c68adf8b31fd8945"
+  license any_of: ["GPL-2.0-or-later", "LGPL-3.0-or-later"]
+
+  depends_on "pkg-config" => :build
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}",
+                          "--disable-csharp",
+                          "--with-lispdir=#{elisp}"
+    system "make", "install"
+  end
+
+  test do
+    ENV["CHARSET"] = "UTF-8"
+    system bin/"idn", "räksmörgås.se", "blåbærgrød.no"
+  end
+end
