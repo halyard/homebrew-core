83075efad84fcf6f486d2c5406b3cfdd0c935c36
Les Aker <me@lesaker.org>
2022-12-11 12:10:03 -0500

    add svn deps

---
+++ Formula/scons.rb
@@ -0,0 +1,29 @@
+class Scons < Formula
+  include Language::Python::Virtualenv
+
+  desc "Substitute for classic 'make' tool with autoconf/automake functionality"
+  homepage "https://www.scons.org/"
+  url "https://files.pythonhosted.org/packages/c6/63/3a87df61a5d8e1b2ba116f4889f3dbc2717ebe2e34c77b2d34e4e6b9deef/SCons-4.4.0.tar.gz"
+  sha256 "7703c4e9d2200b4854a31800c1dbd4587e1fa86e75f58795c740bcfa7eca7eaa"
+  license "MIT"
+
+  depends_on "python@3.11"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <stdio.h>
+      int main()
+      {
+        printf("Homebrew");
+        return 0;
+      }
+    EOS
+    (testpath/"SConstruct").write "Program('test.c')"
+    system bin/"scons"
+    assert_equal "Homebrew", shell_output("#{testpath}/test")
+  end
+end
---
+++ Formula/utf8proc.rb
@@ -0,0 +1,26 @@
+class Utf8proc < Formula
+  desc "Clean C library for processing UTF-8 Unicode data"
+  homepage "https://juliastrings.github.io/utf8proc/"
+  url "https://github.com/JuliaStrings/utf8proc/archive/v2.8.0.tar.gz"
+  sha256 "a0a60a79fe6f6d54e7d411facbfcc867a6e198608f2cd992490e46f04b1bcecc"
+  license all_of: ["MIT", "Unicode-DFS-2015"]
+
+  def install
+    system "make", "install", "prefix=#{prefix}"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <string.h>
+      #include <utf8proc.h>
+
+      int main() {
+        const char *version = utf8proc_version();
+        return strnlen(version, sizeof("1.3.1-dev")) > 0 ? 0 : -1;
+      }
+    EOS
+
+    system ENV.cc, "test.c", "-I#{include}", "-L#{lib}", "-lutf8proc", "-o", "test"
+    system "./test"
+  end
+end
