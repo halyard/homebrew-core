2f7b78cfe282b848dcebc6f1c96421d890f55fa8
Les Aker <me@lesaker.org>
2022-12-22 21:30:28 -0500

    add podman deps

---
+++ Formula/z3.rb
@@ -0,0 +1,62 @@
+class Z3 < Formula
+  desc "High-performance theorem prover"
+  homepage "https://github.com/Z3Prover/z3"
+  url "https://github.com/Z3Prover/z3/archive/z3-4.11.2.tar.gz"
+  sha256 "e3a82431b95412408a9c994466fad7252135c8ed3f719c986cd75c8c5f234c7e"
+  license "MIT"
+  revision 1
+  head "https://github.com/Z3Prover/z3.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+    regex(%r{href=.*?/tag/z3[._-]v?(\d+(?:\.\d+)+)["' >]}i)
+  end
+
+  depends_on "cmake" => :build
+  # Has Python bindings but are supplementary to the main library
+  # which does not need Python.
+  depends_on "python@3.11" => [:build, :test]
+
+  fails_with gcc: "5"
+
+  def python3
+    which("python3.11")
+  end
+
+  def install
+    # LTO on Intel Monterey produces segfaults.
+    # https://github.com/Z3Prover/z3/issues/6414
+    do_lto = MacOS.version < :monterey || Hardware::CPU.arm?
+    args = %W[
+      -DZ3_LINK_TIME_OPTIMIZATION=#{do_lto ? "ON" : "OFF"}
+      -DZ3_INCLUDE_GIT_DESCRIBE=OFF
+      -DZ3_INCLUDE_GIT_HASH=OFF
+      -DZ3_INSTALL_PYTHON_BINDINGS=ON
+      -DZ3_BUILD_EXECUTABLE=ON
+      -DZ3_BUILD_TEST_EXECUTABLES=OFF
+      -DZ3_BUILD_PYTHON_BINDINGS=ON
+      -DZ3_BUILD_DOTNET_BINDINGS=OFF
+      -DZ3_BUILD_JAVA_BINDINGS=OFF
+      -DZ3_USE_LIB_GMP=OFF
+      -DPYTHON_EXECUTABLE=#{python3}
+      -DCMAKE_INSTALL_PYTHON_PKG_DIR=#{Language::Python.site_packages(python3)}
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    system "make", "-C", "contrib/qprofdiff"
+    bin.install "contrib/qprofdiff/qprofdiff"
+
+    pkgshare.install "examples"
+  end
+
+  test do
+    system ENV.cc, pkgshare/"examples/c/test_capi.c", "-I#{include}",
+                   "-L#{lib}", "-lz3", "-o", testpath/"test"
+    system "./test"
+    assert_equal version.to_s, shell_output("#{python3} -c 'import z3; print(z3.get_version_string())'").strip
+  end
+end
