92933701f0ba1d5caf8adf2f014b62a8892e0c43
Les Aker <me@lesaker.org>
2023-01-26 15:39:52 -0500

    update nasm

--- Formula/nasm.rb
+++ Formula/nasm.rb
@@ -1,8 +1,8 @@
 class Nasm < Formula
   desc "Netwide Assembler (NASM) is an 80x86 assembler"
   homepage "https://www.nasm.us/"
-  url "https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/nasm-2.15.05.tar.xz"
-  sha256 "3caf6729c1073bf96629b57cee31eeb54f4f8129b01902c73428836550b30a3f"
+  url "https://www.nasm.us/pub/nasm/releasebuilds/2.16.01/nasm-2.16.01.tar.xz"
+  sha256 "c77745f4802375efeee2ec5c0ad6b7f037ea9c87c92b149a9637ff099f162558"
   license "BSD-2-Clause"
 
   livecheck do
@@ -11,7 +11,7 @@ class Nasm < Formula
   end
 
   head do
-    url "https://github.com/netwide-assembler/nasm.git"
+    url "https://github.com/netwide-assembler/nasm.git", branch: "master"
     depends_on "asciidoc" => :build
     depends_on "autoconf" => :build
     depends_on "automake" => :build
@@ -22,8 +22,7 @@ class Nasm < Formula
     system "./autogen.sh" if build.head?
     system "./configure", "--prefix=#{prefix}"
     system "make", "manpages" if build.head?
-    system "make", "rdf"
-    system "make", "install", "install_rdf"
+    system "make", "install"
   end
 
   test do
