45025f548a463c476146be36d30970176566e7d0
Les Aker <me@lesaker.org>
2023-01-26 14:27:37 -0500

    update cmake

--- Formula/cmake.rb
+++ Formula/cmake.rb
@@ -1,10 +1,10 @@
 class Cmake < Formula
   desc "Cross-platform make"
   homepage "https://www.cmake.org/"
-  url "https://github.com/Kitware/CMake/releases/download/v3.25.1/cmake-3.25.1.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/cmake-3.25.1.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.25.1.tar.gz"
-  sha256 "1c511d09516af493694ed9baf13c55947a36389674d657a2d5e0ccedc6b291d8"
+  url "https://github.com/Kitware/CMake/releases/download/v3.25.2/cmake-3.25.2.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/cmake-3.25.2.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.25.2.tar.gz"
+  sha256 "c026f22cb931dd532f648f087d587f07a1843c6e66a3dfca4fb0ea21944ed33c"
   license "BSD-3-Clause"
   head "https://gitlab.kitware.com/cmake/cmake.git", branch: "master"
 
