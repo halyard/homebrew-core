bc176274a4ed509706cf4bb712e78701d42075bc
Les Aker <me@lesaker.org>
2023-01-27 13:21:43 -0500

    add tfsec

---
+++ Formula/tfsec.rb
@@ -0,0 +1,41 @@
+class Tfsec < Formula
+  desc "Static analysis security scanner for your terraform code"
+  homepage "https://tfsec.dev/"
+  url "https://github.com/aquasecurity/tfsec/archive/v1.28.1.tar.gz"
+  sha256 "17c12304ae34bdb0cc9de916203a45077a96a2786084edc6bebf7b769dc430e8"
+  license "MIT"
+  head "https://github.com/aquasecurity/tfsec.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "go" => :build
+
+  def install
+    system "scripts/install.sh", "v#{version}"
+    bin.install "tfsec"
+  end
+
+  test do
+    (testpath/"good/brew-validate.tf").write <<~EOS
+      resource "aws_alb_listener" "my-alb-listener" {
+        port     = "443"
+        protocol = "HTTPS"
+      }
+    EOS
+    (testpath/"bad/brew-validate.tf").write <<~EOS
+      resource "aws_security_group_rule" "world" {
+        description = "A security group triggering tfsec AWS006."
+        type        = "ingress"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+    EOS
+
+    good_output = shell_output("#{bin}/tfsec #{testpath}/good")
+    assert_match "No problems detected!", good_output
+    bad_output = shell_output("#{bin}/tfsec #{testpath}/bad 2>&1", 1)
+    assert_match "1 potential problem(s) detected.", bad_output
+  end
+end
