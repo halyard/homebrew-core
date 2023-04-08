d03c11a7bc3de56b8e7203bf1c7e5d53aac0ff8d
Les Aker <me@lesaker.org>
2023-04-08 10:57:38 -0400

    update xz

--- Formula/xz.rb
+++ Formula/xz.rb
@@ -4,11 +4,11 @@ class Xz < Formula
   desc "General-purpose data compression with high compression ratio"
   homepage "https://tukaani.org/xz/"
   # The archive.org mirror below needs to be manually created at `archive.org`.
-  url "https://downloads.sourceforge.net/project/lzmautils/xz-5.4.1.tar.gz"
-  mirror "https://tukaani.org/xz/xz-5.4.1.tar.gz"
-  mirror "https://archive.org/download/xz-5.4.1/xz-5.4.1.tar.gz"
-  mirror "http://archive.org/download/xz-5.4.1/xz-5.4.1.tar.gz"
-  sha256 "e4b0f81582efa155ccf27bb88275254a429d44968e488fc94b806f2a61cd3e22"
+  url "https://downloads.sourceforge.net/project/lzmautils/xz-5.4.2.tar.gz"
+  mirror "https://tukaani.org/xz/xz-5.4.2.tar.gz"
+  mirror "https://archive.org/download/xz-5.4.2/xz-5.4.2.tar.gz"
+  mirror "http://archive.org/download/xz-5.4.2/xz-5.4.2.tar.gz"
+  sha256 "87947679abcf77cc509d8d1b474218fd16b72281e2797360e909deaee1ac9d05"
   license all_of: [
     :public_domain,
     "LGPL-2.1-or-later",
