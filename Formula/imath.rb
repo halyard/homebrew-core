e3e53aef452eab254fda419449af848cc38218ad
Les Aker <me@lesaker.org>
2023-04-08 10:36:27 -0400

    update imath

--- Formula/imath.rb
+++ Formula/imath.rb
@@ -1,8 +1,8 @@
 class Imath < Formula
   desc "Library of 2D and 3D vector, matrix, and math operations"
   homepage "https://www.openexr.com/"
-  url "https://github.com/AcademySoftwareFoundation/Imath/archive/refs/tags/v3.1.6.tar.gz"
-  sha256 "ea5592230f5ab917bea3ceab266cf38eb4aa4a523078d46eac0f5a89c52304db"
+  url "https://github.com/AcademySoftwareFoundation/Imath/archive/refs/tags/v3.1.7.tar.gz"
+  sha256 "bff1fa140f4af0e7f02c6cb78d41b9a7d5508e6bcdfda3a583e35460eb6d4b47"
   license "BSD-3-Clause"
 
   depends_on "cmake" => :build
