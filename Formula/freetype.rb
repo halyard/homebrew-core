25f0bd84803dfbfc32aa22755ce481454134088b
Les Aker <me@lesaker.org>
2023-04-08 10:21:52 -0400

    update freetype

--- Formula/freetype.rb
+++ Formula/freetype.rb
@@ -1,22 +1,41 @@
 class Freetype < Formula
   desc "Software library to render fonts"
   homepage "https://www.freetype.org/"
-  url "https://downloads.sourceforge.net/project/freetype/freetype2/2.12.1/freetype-2.12.1.tar.xz"
-  mirror "https://download.savannah.gnu.org/releases/freetype/freetype-2.12.1.tar.xz"
-  sha256 "4766f20157cc4cf0cd292f80bf917f92d1c439b243ac3018debf6b9140c41a7f"
+  url "https://downloads.sourceforge.net/project/freetype/freetype2/2.13.0/freetype-2.13.0.tar.xz"
+  mirror "https://download.savannah.gnu.org/releases/freetype/freetype-2.13.0.tar.xz"
+  sha256 "5ee23abd047636c24b2d43c6625dcafc66661d1aca64dec9e0d05df29592624c"
   license "FTL"
+  revision 1
 
   livecheck do
     url :stable
     regex(/url=.*?freetype[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
+  depends_on "pkg-config" => :build
   depends_on "libpng"
 
   uses_from_macos "bzip2"
   uses_from_macos "zlib"
 
+  # Earlier versions of Apple Clang don't recognise the `fallthrough` attribute.
+  # The following patches make it possible to build with them.
+  # Remove both on next release.
+  patch do
+    url "https://gitlab.freedesktop.org/freetype/freetype/-/commit/2257f9abf6e12daf7c3e1bfe28fa88de85e45785.diff"
+    sha256 "64f41363467c455ccfeb3350bc3bea0c028fa5d108821e2e81cd8475675b7926"
+  end
+
+  patch do
+    url "https://gitlab.freedesktop.org/freetype/freetype/-/commit/d874ffa96ccad7dd122cdc369a284d171e221809.diff"
+    sha256 "aff06e28afc48cd96d7ea4321069046db8ba0f512bf965ef475c1cdbcdc2635f"
+  end
+
   def install
+    # This file will be installed to bindir, so we want to avoid embedding the
+    # absolute path to the pkg-config shim.
+    inreplace "builds/unix/freetype-config.in", "%PKG_CONFIG%", "pkg-config"
+
     system "./configure", "--prefix=#{prefix}",
                           "--enable-freetype-config",
                           "--without-harfbuzz"
