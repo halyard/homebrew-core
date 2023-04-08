4e2d07267937cad67a02d9e83f81ac28cdb4c53b
Les Aker <me@lesaker.org>
2023-04-08 10:56:09 -0400

    update treesitter

--- Formula/tree-sitter.rb
+++ Formula/tree-sitter.rb
@@ -3,8 +3,8 @@ require "language/node"
 class TreeSitter < Formula
   desc "Parser generator tool and incremental parsing library"
   homepage "https://tree-sitter.github.io/"
-  url "https://github.com/tree-sitter/tree-sitter/archive/v0.20.7.tar.gz"
-  sha256 "b355e968ec2d0241bbd96748e00a9038f83968f85d822ecb9940cbe4c42e182e"
+  url "https://github.com/tree-sitter/tree-sitter/archive/v0.20.8.tar.gz"
+  sha256 "6181ede0b7470bfca37e293e7d5dc1d16469b9485d13f13a605baec4a8b1f791"
   license "MIT"
   head "https://github.com/tree-sitter/tree-sitter.git", branch: "master"
 
