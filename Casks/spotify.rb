b91ddbb3cc971525872447085d46c5abf0cfe9b4
Les Aker <me@lesaker.org>
2023-04-08 11:42:21 -0400

    update spotify

--- Casks/spotify.rb
+++ Casks/spotify.rb
@@ -3,11 +3,11 @@ cask "spotify" do
 
   sha256 :no_check
 
-  on_intel do
-    version "1.2.3.1115,d61a8f5c,384"
-  end
   on_arm do
-    version "1.2.3.1115,d61a8f5c,384"
+    version "1.2.8.923,4f94bf0d,1547"
+  end
+  on_intel do
+    version "1.2.8.923,4f94bf0d,1545"
   end
 
   url "https://download.scdn.co/Spotify#{arch}.dmg",
