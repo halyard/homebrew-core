77c61c6cddd2e39fa51bf49334ecae6e99df1caa
Les Aker <me@lesaker.org>
2023-04-08 11:41:54 -0400

    update slack

--- Casks/slack.rb
+++ Casks/slack.rb
@@ -1,20 +1,31 @@
 cask "slack" do
   arch arm: "arm64", intel: "x64"
 
-  version "4.29.149"
-  sha256 arm:   "367f9d143dc7fc1cbe6813cb0f19cf9de357b8597eb4774b87125779804c3918",
-         intel: "13462738f9da5857b5ee00a9172c31bb9b87b35a4492d035b54bb842abca858c"
+  on_mojave :or_older do
+    version "4.29.149"
+    sha256 arm:   "367f9d143dc7fc1cbe6813cb0f19cf9de357b8597eb4774b87125779804c3918",
+           intel: "13462738f9da5857b5ee00a9172c31bb9b87b35a4492d035b54bb842abca858c"
+
+    livecheck do
+      skip "Legacy version"
+    end
+  end
+  on_catalina :or_newer do
+    version "4.31.156"
+    sha256 arm:   "301d6875a5388cce5ad7eb78befb30eb7b0262aae1c2aa4580c8ae2f2fafbdcc",
+           intel: "05cb9917a3f85eda4f8623bf0321649e5f1f614cec7a16a7998d9838b2c12ffe"
+
+    livecheck do
+      url "https://slack.com/ssb/download-osx"
+      strategy :header_match
+    end
+  end
 
   url "https://downloads.slack-edge.com/releases/macos/#{version}/prod/#{arch}/Slack-#{version}-macOS.dmg",
       verified: "downloads.slack-edge.com/releases/macos/"
   name "Slack"
   desc "Team communication and collaboration software"
   homepage "https://slack.com/"
-
-  livecheck do
-    url "https://slack.com/ssb/download-osx"
-    strategy :header_match
-  end
 
   auto_updates true
   conflicts_with cask: "homebrew/cask-versions/slack-beta"
