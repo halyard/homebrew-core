c92fc0694d4a1dc4a80e4ebad8b5f10c362d3dce
Les Aker <me@lesaker.org>
2023-04-08 10:49:25 -0400

    update pandoc

--- Formula/pandoc.rb
+++ Formula/pandoc.rb
@@ -1,8 +1,8 @@
 class Pandoc < Formula
   desc "Swiss-army knife of markup format conversion"
   homepage "https://pandoc.org/"
-  url "https://github.com/jgm/pandoc/archive/refs/tags/3.0.1.tar.gz"
-  sha256 "d8b45c3d8434c03db7cd6c571664a2a0a0ba906bfee3b8d82470103978ddc4ce"
+  url "https://github.com/jgm/pandoc/archive/refs/tags/3.1.2.tar.gz"
+  sha256 "7543c31e09ad8ccb2ce381c4039d76823865dc6c3922bfa2ed49367cf0e1fd3b"
   license "GPL-2.0-or-later"
   head "https://github.com/jgm/pandoc.git", branch: "master"
 
