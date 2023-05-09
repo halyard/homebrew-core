5f66d65c79cdaa6ebf4db2aa53c9810f1e7eba31
Les Aker <me@lesaker.org>
2023-05-09 00:50:30 -0400

    add missing formula deps

---
+++ Formula/cargo-c.rb
@@ -0,0 +1,57 @@
+class CargoC < Formula
+  desc "Helper program to build and install c-like libraries"
+  homepage "https://github.com/lu-zero/cargo-c"
+  url "https://github.com/lu-zero/cargo-c/archive/refs/tags/v0.9.19.tar.gz"
+  sha256 "c2633ff22e52da9985394f30c8ef5e9abbac4d14c9b79e3690c8e95cf500ab97"
+  license "MIT"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "rust" => :build
+  depends_on "libgit2@1.5"
+  depends_on "libssh2"
+  depends_on "openssl@1.1"
+
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "pkg-config" => :build
+  end
+
+  def install
+    ENV["LIBGIT2_SYS_USE_PKG_CONFIG"] = "1"
+    ENV["LIBSSH2_SYS_USE_PKG_CONFIG"] = "1"
+    # Ensure the correct `openssl` will be picked up.
+    ENV["OPENSSL_DIR"] = Formula["openssl@1.1"].opt_prefix
+    ENV["OPENSSL_NO_VENDOR"] = "1"
+
+    system "cargo", "install", *std_cargo_args
+  end
+
+  def check_binary_linkage(binary, library)
+    binary.dynamically_linked_libraries.any? do |dll|
+      next false unless dll.start_with?(HOMEBREW_PREFIX.to_s)
+
+      File.realpath(dll) == File.realpath(library)
+    end
+  end
+
+  test do
+    cargo_error = "could not find `Cargo.toml`"
+    assert_match cargo_error, shell_output("#{bin}/cargo-cinstall cinstall 2>&1", 1)
+    assert_match cargo_error, shell_output("#{bin}/cargo-cbuild cbuild 2>&1", 1)
+
+    [
+      Formula["libgit2@1.5"].opt_lib/shared_library("libgit2"),
+      Formula["libssh2"].opt_lib/shared_library("libssh2"),
+      Formula["openssl@1.1"].opt_lib/shared_library("libssl"),
+      Formula["openssl@1.1"].opt_lib/shared_library("libcrypto"),
+    ].each do |library|
+      assert check_binary_linkage(bin/"cargo-cbuild", library),
+             "No linkage with #{library.basename}! Cargo is likely using a vendored version."
+    end
+  end
+end
---
+++ Formula/libgit2@1.5.rb
@@ -0,0 +1,48 @@
+class Libgit2AT15 < Formula
+  desc "C library of Git core methods that is re-entrant and linkable"
+  homepage "https://libgit2.github.com/"
+  url "https://github.com/libgit2/libgit2/archive/v1.5.2.tar.gz"
+  sha256 "57638ac0e319078f56a7e17570be754515e5b1276d3750904b4214c92e8fa196"
+  license "GPL-2.0-only" => { with: "GCC-exception-2.0" }
+  head "https://github.com/libgit2/libgit2.git", branch: "maint/v1.5"
+
+  livecheck do
+    url :stable
+    regex(/v?(1\.5(?:\.\d+)+)/i)
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libssh2"
+
+  def install
+    args = %w[-DBUILD_EXAMPLES=OFF -DBUILD_TESTS=OFF -DUSE_SSH=ON -DBUILD_SHARED_LIBS=ON]
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <git2.h>
+      #include <assert.h>
+
+      int main(int argc, char *argv[]) {
+        int options = git_libgit2_features();
+        assert(options & GIT_FEATURE_SSH);
+        return 0;
+      }
+    EOS
+    libssh2 = Formula["libssh2"]
+    flags = %W[
+      -I#{include}
+      -I#{libssh2.opt_include}
+      -L#{lib}
+      -lgit2
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+  end
+end
