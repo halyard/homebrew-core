64a0cdb6b8435e52bf9790b64710383408ea4fba
Les Aker <me@lesaker.org>
2023-07-11 23:18:04 -0400

    update psutils

--- Formula/psutils.rb
+++ Formula/psutils.rb
@@ -1,39 +1,43 @@
 class Psutils < Formula
-  desc "Collection of PostScript document handling utilities"
-  homepage "http://knackered.org/angus/psutils/"
-  url "ftp://ftp.knackered.org/pub/psutils/psutils-p17.tar.gz"
-  mirror "https://ftp.osuosl.org/pub/blfs/conglomeration/psutils/psutils-p17.tar.gz"
-  version "p17"
-  sha256 "3853eb79584ba8fbe27a815425b65a9f7f15b258e0d43a05a856bdb75d588ae4"
-  license "psutils"
+  include Language::Python::Virtualenv
 
-  # This regex is open-ended (i.e., it doesn't contain a trailing delimiter like
-  # `\.t`), since the homepage only links to an unversioned archive file
-  # (`psutils.tar.gz`) or a versioned archive file with additional trailing text
-  # (`psutils-p17-a4-nt.zip`). Relying on the trailing text of the versioned
-  # archive file remaining the same could make this check liable to break, so
-  # we'll simply leave it looser until/unless it causes a problem.
-  livecheck do
-    url :homepage
-    regex(/href=.*?psutils[._-](p\d+)/i)
+  desc "Utilities for manipulating PostScript documents"
+  homepage "https://github.com/rrthomas/psutils"
+  url "https://files.pythonhosted.org/packages/30/70/f1f0f2744ac0a7a0fad4bee698ef903278080afffdab6c557cf247fa2924/pspdfutils-3.0.6.tar.gz"
+  sha256 "06501dac26044eaafe40fb550281f057b59f3ab565f75dcb9d0590184d3416fc"
+  license "GPL-3.0-or-later"
+
+  depends_on "libpaper"
+  depends_on "python@3.11"
+
+  resource "puremagic" do
+    url "https://files.pythonhosted.org/packages/50/bb/c9860ce714ce2147b6168fdf817e67c3be6eabc822fab5ef41cc52bafdec/puremagic-1.15.tar.gz"
+    sha256 "6e46aa78113a466abc9f69e6e8a4ce90eb57d908dafb809597012621061462bd"
+  end
+
+  resource "pypdf" do
+    url "https://files.pythonhosted.org/packages/8a/c6/83cce3fed4c14d0c0f96fd938430516f9371f96fa5801d59d1ba007c8fd8/pypdf-3.12.0.tar.gz"
+    sha256 "cebac920db0698369f49c389018858a5436862bf3c45b64b10c55c008878db95"
   end
 
   def install
-    # This is required, because the makefile expects that its man folder exists
-    man1.mkpath
-    system "make", "-f", "Makefile.unix",
-                         "PERL=/usr/bin/perl",
-                         "BINDIR=#{bin}",
-                         "INCLUDEDIR=#{pkgshare}",
-                         "MANDIR=#{man1}",
-                         "install"
+    virtualenv_install_with_resources
   end
 
   test do
-    system "sh", "-c", "#{bin}/showchar Palatino B > test.ps"
-    system "#{bin}/psmerge", "-omulti.ps", "test.ps", "test.ps",
-      "test.ps", "test.ps"
-    system "#{bin}/psnup", "-n", "2", "multi.ps", "nup.ps"
-    system "#{bin}/psselect", "-p1", "multi.ps", "test2.ps"
+    resource "homebrew-test-ps" do
+      url "https://raw.githubusercontent.com/rrthomas/psutils/e00061c21e114d80fbd5073a4509164f3799cc24/tests/test-files/psbook/3/expected.ps"
+      sha256 "bf3f1b708c3e6a70d0f28af55b3b511d2528b98c2a1537674439565cecf0aed6"
+    end
+    resource("homebrew-test-ps").stage testpath
+
+    expected_psbook_output = "[4] [1] [2] [3] \nWrote 4 pages\n"
+    assert_equal expected_psbook_output, shell_output("#{bin}/psbook expected.ps book.ps 2>&1")
+
+    expected_psnup_output = "[1,2] [3,4] \nWrote 2 pages\n"
+    assert_equal expected_psnup_output, shell_output("#{bin}/psnup -2 expected.ps nup.ps 2>&1")
+
+    expected_psselect_output = "[1] \nWrote 1 pages\n"
+    assert_equal expected_psselect_output, shell_output("#{bin}/psselect -p1 expected.ps test2.ps 2>&1")
   end
 end
