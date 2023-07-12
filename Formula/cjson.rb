40613a72dcf1665e25d0dcfd292b660fb30fa2e8
Les Aker <me@lesaker.org>
2023-07-11 22:43:04 -0400

    update c-json

--- Formula/cjson.rb
+++ Formula/cjson.rb
@@ -1,17 +1,21 @@
 class Cjson < Formula
   desc "Ultralightweight JSON parser in ANSI C"
   homepage "https://github.com/DaveGamble/cJSON"
-  url "https://github.com/DaveGamble/cJSON/archive/v1.7.15.tar.gz"
-  sha256 "5308fd4bd90cef7aa060558514de6a1a4a0819974a26e6ed13973c5f624c24b2"
+  url "https://github.com/DaveGamble/cJSON/archive/v1.7.16.tar.gz"
+  sha256 "451131a92c55efc5457276807fc0c4c2c2707c9ee96ef90c47d68852d5384c6c"
   license "MIT"
 
   depends_on "cmake" => :build
 
   def install
-    system "cmake", "-DENABLE_CJSON_UTILS=On", "-DENABLE_CJSON_TEST=Off",
-                    "-DBUILD_SHARED_AND_STATIC_LIBS=On", ".",
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DENABLE_CJSON_UTILS=ON",
+                    "-DENABLE_CJSON_TEST=Off",
+                    "-DBUILD_SHARED_AND_STATIC_LIBS=ON",
+                    "-DCMAKE_INSTALL_RPATH=#{rpath}",
                     *std_cmake_args
-    system "make", "install"
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
