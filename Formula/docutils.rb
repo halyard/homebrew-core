22ed7c0f2e7ce72d1f4b745851f3b432e9888343
Les Aker <me@lesaker.org>
2023-07-11 22:46:15 -0400

    update docutils

--- Formula/docutils.rb
+++ Formula/docutils.rb
@@ -1,54 +1,26 @@
 class Docutils < Formula
   desc "Text processing system for reStructuredText"
   homepage "https://docutils.sourceforge.io"
-  url "https://downloads.sourceforge.net/project/docutils/docutils/0.19/docutils-0.19.tar.gz"
-  sha256 "33995a6753c30b7f577febfc2c50411fec6aac7f7ffeb7c4cfe5991072dcf9e6"
+  url "https://downloads.sourceforge.net/project/docutils/docutils/0.20.1/docutils-0.20.1.tar.gz"
+  sha256 "f08a4e276c3a1583a86dce3e34aba3fe04d02bba2dd51ed16106244e8a923e3b"
   license all_of: [:public_domain, "BSD-2-Clause", "GPL-3.0-or-later", "Python-2.0"]
 
-  depends_on "python@3.10" => [:build, :test]
-  depends_on "python@3.11" => [:build, :test]
-
-  def pythons
-    deps.map(&:to_formula)
-        .select { |f| f.name.match?(/^python@3\.\d+$/) }
-  end
+  depends_on "python@3.11"
 
   def install
-    pythons.each do |python|
-      python_exe = python.opt_libexec/"bin/python"
-      system python_exe, *Language::Python.setup_install_args(libexec, python_exe)
+    python3 = "python3.11"
+    system python3, *Language::Python.setup_install_args(prefix, python3)
 
-      site_packages = Language::Python.site_packages(python_exe)
-      pth_contents = "import site; site.addsitedir('#{libexec/site_packages}')\n"
-      (prefix/site_packages/"homebrew-docutils.pth").write pth_contents
-
-      pyversion = Language::Python.major_minor_version(python_exe)
-      Dir.glob("#{libexec}/bin/*.py") do |f|
-        bname = File.basename(f, ".py")
-        bin.install_symlink f => "#{bname}-#{pyversion}"
-        bin.install_symlink f => "#{bname}.py-#{pyversion}"
-      end
-
-      next unless python == pythons.max_by(&:version)
-
-      # The newest one is used as the default
-      Dir.glob("#{libexec}/bin/*.py") do |f|
-        bname = File.basename(f, ".py")
-        bin.install_symlink f => bname
-        bin.install_symlink f => "#{bname}.py"
-      end
+    bin.glob("*.py") do |f|
+      bin.install_symlink f => f.basename(".py")
     end
   end
 
   test do
-    pythons.each do |python|
-      python_exe = python.opt_libexec/"bin/python"
-      pyversion = Language::Python.major_minor_version(python_exe)
-      system "#{bin}/rst2man.py-#{pyversion}", "#{prefix}/HISTORY.txt"
-      system "#{bin}/rst2man-#{pyversion}", "#{prefix}/HISTORY.txt"
-    end
-
-    system "#{bin}/rst2man.py", "#{prefix}/HISTORY.txt"
-    system "#{bin}/rst2man", "#{prefix}/HISTORY.txt"
+    cp prefix/"README.txt", testpath
+    mkdir_p testpath/"docs"
+    touch testpath/"docs"/"header0.txt"
+    system bin/"rst2man.py", testpath/"README.txt"
+    system bin/"rst2man", testpath/"README.txt"
   end
 end
