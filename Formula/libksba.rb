5d4f71b23f6cfa8a5585fac83706e9d04f0491ce
Les Aker <me@lesaker.org>
2023-07-11 23:00:28 -0400

    update libksba

--- Formula/libksba.rb
+++ Formula/libksba.rb
@@ -1,8 +1,8 @@
 class Libksba < Formula
   desc "X.509 and CMS library"
   homepage "https://www.gnupg.org/related_software/libksba/"
-  url "https://gnupg.org/ftp/gcrypt/libksba/libksba-1.6.3.tar.bz2"
-  sha256 "3f72c68db30971ebbf14367527719423f0a4d5f8103fc9f4a1c01a9fa440de5c"
+  url "https://gnupg.org/ftp/gcrypt/libksba/libksba-1.6.4.tar.bz2"
+  sha256 "bbb43f032b9164d86c781ffe42213a83bf4f2fee91455edfa4654521b8b03b6b"
   license any_of: ["LGPL-3.0-or-later", "GPL-2.0-or-later"]
 
   livecheck do
@@ -16,13 +16,27 @@ class Libksba < Formula
     system "./configure", "--disable-dependency-tracking",
                           "--disable-silent-rules",
                           "--prefix=#{prefix}"
+    system "make", "check"
     system "make", "install"
 
     # avoid triggering mandatory rebuilds of software that hard-codes this path
-    inreplace bin/"ksba-config", prefix, opt_prefix
+    inreplace [bin/"ksba-config", lib/"pkgconfig/ksba.pc"], prefix, opt_prefix
   end
 
   test do
-    system "#{bin}/ksba-config", "--libs"
+    (testpath/"ksba-test.c").write <<~C
+      #include "ksba.h"
+      #include <stdio.h>
+      int main() {
+        printf("%s", ksba_check_version(NULL));
+        return 0;
+      }
+    C
+
+    ENV.append_to_cflags shell_output("#{bin}/ksba-config --cflags").strip
+    ENV.append "LDLIBS", shell_output("#{bin}/ksba-config --libs").strip
+
+    system "make", "ksba-test"
+    assert_equal version.to_s, shell_output("./ksba-test")
   end
 end
