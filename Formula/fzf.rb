efdb06222e1f480359e484a9f20c0070b11137f0
Les Aker <me@lesaker.org>
2023-07-11 22:49:14 -0400

    update fzf

--- Formula/fzf.rb
+++ Formula/fzf.rb
@@ -1,8 +1,8 @@
 class Fzf < Formula
   desc "Command-line fuzzy finder written in Go"
   homepage "https://github.com/junegunn/fzf"
-  url "https://github.com/junegunn/fzf/archive/0.39.0.tar.gz"
-  sha256 "ac665ac269eca320ca9268227142f01b10ad5d25364ff274658b5a9f709a7259"
+  url "https://github.com/junegunn/fzf/archive/0.42.0.tar.gz"
+  sha256 "743c1bfc7851b0796ab73c6da7db09d915c2b54c0dd3e8611308985af8ed3df2"
   license "MIT"
   head "https://github.com/junegunn/fzf.git", branch: "master"
 
