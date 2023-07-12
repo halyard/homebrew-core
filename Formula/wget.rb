a1ea46f6709814f19e839be1a179d9c6e941d4a9
Les Aker <me@lesaker.org>
2023-07-11 23:30:26 -0400

    update wget

--- Formula/wget.rb
+++ Formula/wget.rb
@@ -1,8 +1,8 @@
 class Wget < Formula
   desc "Internet file retriever"
   homepage "https://www.gnu.org/software/wget/"
-  url "https://ftp.gnu.org/gnu/wget/wget-1.21.3.tar.gz"
-  sha256 "5726bb8bc5ca0f6dc7110f6416e4bb7019e2d2ff5bf93d1ca2ffcc6656f220e5"
+  url "https://ftp.gnu.org/gnu/wget/wget-1.21.4.tar.gz"
+  sha256 "81542f5cefb8faacc39bbbc6c82ded80e3e4a88505ae72ea51df27525bcde04c"
   license "GPL-3.0-or-later"
 
   head do
@@ -16,7 +16,7 @@ class Wget < Formula
 
   depends_on "pkg-config" => :build
   depends_on "libidn2"
-  depends_on "openssl@1.1"
+  depends_on "openssl@3"
 
   on_linux do
     depends_on "util-linux"
@@ -27,7 +27,7 @@ class Wget < Formula
     system "./configure", "--prefix=#{prefix}",
                           "--sysconfdir=#{etc}",
                           "--with-ssl=openssl",
-                          "--with-libssl-prefix=#{Formula["openssl@1.1"].opt_prefix}",
+                          "--with-libssl-prefix=#{Formula["openssl@3"].opt_prefix}",
                           "--disable-pcre",
                           "--disable-pcre2",
                           "--without-libpsl",
