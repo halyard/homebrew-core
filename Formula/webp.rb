56998e88feb10a7cbac480dbc704ad13f1cc5d19
Les Aker <me@lesaker.org>
2023-07-11 23:30:17 -0400

    update webp

--- Formula/webp.rb
+++ Formula/webp.rb
@@ -1,31 +1,27 @@
 class Webp < Formula
   desc "Image format providing lossless and lossy compression for web images"
   homepage "https://developers.google.com/speed/webp/"
-  url "https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.3.0.tar.gz"
-  sha256 "64ac4614db292ae8c5aa26de0295bf1623dbb3985054cb656c55e67431def17c"
+  url "https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-1.3.1.tar.gz"
+  sha256 "b3779627c2dfd31e3d8c4485962c2efe17785ef975e2be5c8c0c9e6cd3c4ef66"
   license "BSD-3-Clause"
+  head "https://chromium.googlesource.com/webm/libwebp.git", branch: "main"
 
-  head do
-    url "https://chromium.googlesource.com/webm/libwebp.git", branch: "main"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
-
+  depends_on "cmake" => :build
   depends_on "giflib"
   depends_on "jpeg-turbo"
   depends_on "libpng"
   depends_on "libtiff"
 
   def install
-    system "./autogen.sh" if build.head?
-    system "./configure", "--prefix=#{prefix}",
-                          "--disable-dependency-tracking",
-                          "--disable-gl",
-                          "--enable-libwebpdecoder",
-                          "--enable-libwebpdemux",
-                          "--enable-libwebpmux"
-    system "make", "install"
+    args = %W[
+      -DCMAKE_INSTALL_RPATH=#{rpath}
+    ]
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, "-DBUILD_SHARED_LIBS=ON", *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+    system "cmake", "-S", ".", "-B", "static", *std_cmake_args, "-DBUILD_SHARED_LIBS=OFF", *args
+    system "cmake", "--build", "static"
+    lib.install buildpath.glob("static/*.a")
   end
 
   test do
