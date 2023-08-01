042cbb53a635f1281d064c407df913c8f85e47ba
Les Aker <me@lesaker.org>
2023-07-31 21:29:28 -0400

    add trivy

---
+++ Formula/trivy.rb
@@ -0,0 +1,21 @@
+class Trivy < Formula
+  desc "Vulnerability scanner for container images, file systems, and Git repos"
+  homepage "https://aquasecurity.github.io/trivy/"
+  url "https://github.com/aquasecurity/trivy/archive/refs/tags/v0.43.1.tar.gz"
+  sha256 "c8e55d945c94a0c6177680b0953427883650638e1588a5e20e61e762c902e213"
+  license "Apache-2.0"
+  head "https://github.com/aquasecurity/trivy.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X=main.version=#{version}"), "./cmd/trivy"
+  end
+
+  test do
+    output = shell_output("#{bin}/trivy image alpine:3.10")
+    assert_match(/\(UNKNOWN: \d+, LOW: \d+, MEDIUM: \d+, HIGH: \d+, CRITICAL: \d+\)/, output)
+
+    assert_match version.to_s, shell_output("#{bin}/trivy --version")
+  end
+end
