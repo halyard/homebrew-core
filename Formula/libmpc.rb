01d76dabeb11548e92c06da75a43f6c694d971c7
Les Aker <me@lesaker.org>
2024-04-02 22:42:17 -0400

    add libmpc

---
+++ Formula/libmpc.rb
@@ -0,0 +1,50 @@
+class Libmpc < Formula
+  desc "C library for the arithmetic of high precision complex numbers"
+  homepage "https://www.multiprecision.org/"
+  url "https://ftp.gnu.org/gnu/mpc/mpc-1.3.1.tar.gz"
+  mirror "https://ftpmirror.gnu.org/mpc/mpc-1.3.1.tar.gz"
+  sha256 "ab642492f5cf882b74aa0cb730cd410a81edcdbec895183ce930e706c1c759b8"
+  license "LGPL-3.0-or-later"
+
+  head do
+    url "https://gitlab.inria.fr/mpc/mpc.git", branch: "master"
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "gmp"
+  depends_on "mpfr"
+
+  def install
+    system "autoreconf", "--force", "--install", "--verbose" if build.head?
+    system "./configure", *std_configure_args,
+                          "--with-gmp=#{Formula["gmp"].opt_prefix}",
+                          "--with-mpfr=#{Formula["mpfr"].opt_prefix}"
+    system "make"
+    system "make", "check"
+    system "make", "install"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <mpc.h>
+      #include <assert.h>
+      #include <math.h>
+
+      int main() {
+        mpc_t x;
+        mpc_init2 (x, 256);
+        mpc_set_d_d (x, 1., INFINITY, MPC_RNDNN);
+        mpc_tanh (x, x, MPC_RNDNN);
+        assert (mpfr_nan_p (mpc_realref (x)) && mpfr_nan_p (mpc_imagref (x)));
+        mpc_clear (x);
+        return 0;
+      }
+    EOS
+    system ENV.cc, "test.c", "-L#{lib}", "-L#{Formula["mpfr"].opt_lib}",
+                   "-L#{Formula["gmp"].opt_lib}", "-lmpc", "-lmpfr",
+                   "-lgmp", "-o", "test"
+    system "./test"
+  end
+end
