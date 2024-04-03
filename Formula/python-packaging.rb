2026c1e348424474f93ed2b7b5f8888ae84943b0
Les Aker <me@lesaker.org>
2024-04-02 22:40:01 -0400

    add python-packaging

---
+++ Formula/python-packaging.rb
@@ -0,0 +1,29 @@
+class PythonPackaging < Formula
+  desc "Core utilities for Python packages"
+  homepage "https://packaging.pypa.io/"
+  url "https://files.pythonhosted.org/packages/ee/b5/b43a27ac7472e1818c4bafd44430e69605baefe1f34440593e0332ec8b4d/packaging-24.0.tar.gz"
+  sha256 "eb82c5e3e56209074766e6885bb04b8c38a0c015d0a30036ebe7ece34c9989e9"
+  license any_of: ["Apache-2.0", "BSD-2-Clause"]
+
+  depends_on "python-flit-core" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+
+  def pythons
+    deps.map(&:to_formula)
+        .select { |f| f.name.start_with?("python@") }
+        .map { |f| f.opt_libexec/"bin/python" }
+  end
+
+  def install
+    pythons.each do |python|
+      system python, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      system python, "-c", "import packaging"
+    end
+  end
+end
