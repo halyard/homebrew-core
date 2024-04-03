adfcb30615554b579f194280b9b87a53a1735bcc
Les Aker <me@lesaker.org>
2024-04-02 22:37:55 -0400

    add libmicrohttpd

---
+++ Formula/libmicrohttpd.rb
@@ -0,0 +1,28 @@
+class Libmicrohttpd < Formula
+  desc "Light HTTP/1.1 server library"
+  homepage "https://www.gnu.org/software/libmicrohttpd/"
+  url "https://ftp.gnu.org/gnu/libmicrohttpd/libmicrohttpd-1.0.1.tar.gz"
+  mirror "https://ftpmirror.gnu.org/libmicrohttpd/libmicrohttpd-1.0.1.tar.gz"
+  sha256 "a89e09fc9b4de34dde19f4fcb4faaa1ce10299b9908db1132bbfa1de47882b94"
+  license "LGPL-2.1-or-later"
+
+  depends_on "gnutls"
+
+  def install
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--enable-https",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+    (pkgshare/"examples").install Dir.glob("doc/examples/*.c")
+  end
+
+  test do
+    cp pkgshare/"examples/simplepost.c", testpath
+    inreplace "simplepost.c",
+      "return 0",
+      "printf(\"daemon %p\", daemon) ; return 0"
+    system ENV.cc, "-o", "foo", "simplepost.c", "-I#{include}", "-L#{lib}", "-lmicrohttpd"
+    assert_match(/daemon 0x[0-9a-f]+[1-9a-f]+/, pipe_output("./foo"))
+  end
+end
