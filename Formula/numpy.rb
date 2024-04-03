3b519c37df772fad7188b45818fb8a44804e9046
Les Aker <me@lesaker.org>
2024-04-02 22:38:48 -0400

    add numpy

---
+++ Formula/numpy.rb
@@ -0,0 +1,59 @@
+class Numpy < Formula
+  desc "Package for scientific computing with Python"
+  homepage "https://www.numpy.org/"
+  license "BSD-3-Clause"
+  head "https://github.com/numpy/numpy.git", branch: "main"
+
+  stable do
+    url "https://files.pythonhosted.org/packages/65/6e/09db70a523a96d25e115e71cc56a6f9031e7b8cd166c1ac8438307c14058/numpy-1.26.4.tar.gz"
+    sha256 "2a02aba9ed12e4ac4eb3ea9421c420301a0c6460d9830d74a9df87efa4912010"
+
+    depends_on "python-setuptools" => :build
+  end
+
+  depends_on "gcc" => :build # for gfortran
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+  depends_on "openblas"
+
+  on_linux do
+    depends_on "patchelf" => :build
+  end
+
+  fails_with gcc: "5"
+
+  def pythons
+    deps.map(&:to_formula)
+        .select { |f| f.name.start_with?("python@") }
+        .sort_by(&:version) # so that `bin/f2py` and `bin/f2py3` use newest python
+  end
+
+  def install
+    pythons.each do |python|
+      python3 = python.opt_libexec/"bin/python"
+      system python3, "-m", "pip", "install", "-Csetup-args=-Dblas=openblas",
+                                              "-Csetup-args=-Dlapack=openblas",
+                                              *std_pip_args(build_isolation: true), "."
+    end
+  end
+
+  def caveats
+    <<~EOS
+      To run `f2py`, you may need to `brew install #{pythons.last}`
+    EOS
+  end
+
+  test do
+    pythons.each do |python|
+      python_exe = python.opt_libexec/"bin/python"
+      system python_exe, "-c", <<~EOS
+        import numpy as np
+        t = np.ones((3,3), int)
+        assert t.sum() == 9
+        assert np.dot(t, t).sum() == 27
+      EOS
+    end
+  end
+end
