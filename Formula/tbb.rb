f713e3ba1b566623420247f0d3a3a1114fcdab6b
Les Aker <me@lesaker.org>
2024-04-02 22:40:32 -0400

    add tbb

---
+++ Formula/tbb.rb
@@ -0,0 +1,105 @@
+class Tbb < Formula
+  desc "Rich and complete approach to parallelism in C++"
+  homepage "https://github.com/oneapi-src/oneTBB"
+  url "https://github.com/oneapi-src/oneTBB/archive/refs/tags/v2021.11.0.tar.gz"
+  sha256 "782ce0cab62df9ea125cdea253a50534862b563f1d85d4cda7ad4e77550ac363"
+  license "Apache-2.0"
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.12" => [:build, :test]
+  depends_on "swig" => :build
+  depends_on "hwloc"
+
+  def python3
+    "python3.12"
+  end
+
+  def install
+    # Prevent `setup.py` from installing tbb4py directly into HOMEBREW_PREFIX.
+    # We need this due to our Python patch.
+    site_packages = Language::Python.site_packages(python3)
+    inreplace "python/CMakeLists.txt", "install --prefix build -f", "\\0 --install-lib build/#{site_packages}"
+
+    tbb_site_packages = prefix/site_packages/"tbb"
+    ENV.append "LDFLAGS", "-Wl,-rpath,#{rpath},-rpath,#{rpath(source: tbb_site_packages)}"
+
+    args = %w[
+      -DTBB_TEST=OFF
+      -DTBB4PY_BUILD=ON
+    ]
+
+    system "cmake", "-S", ".", "-B", "build/shared",
+                    "-DBUILD_SHARED_LIBS=ON",
+                    "-DCMAKE_INSTALL_RPATH=#{rpath}",
+                    "-DPYTHON_EXECUTABLE=#{which(python3)}",
+                    *args, *std_cmake_args
+    system "cmake", "--build", "build/shared"
+    system "cmake", "--install", "build/shared"
+
+    system "cmake", "-S", ".", "-B", "build/static",
+                    "-DBUILD_SHARED_LIBS=OFF",
+                    "-DPYTHON_EXECUTABLE=#{which(python3)}",
+                    *args, *std_cmake_args
+    system "cmake", "--build", "build/static"
+    lib.install buildpath.glob("build/static/*/libtbb*.a")
+
+    ENV.append_path "CMAKE_PREFIX_PATH", prefix.to_s
+    ENV["TBBROOT"] = prefix
+    system python3, "-m", "pip", "install", *std_pip_args, "./python"
+  end
+
+  test do
+    # The glob that installs these might fail,
+    # so let's check their existence.
+    assert_path_exists lib/"libtbb.a"
+    assert_path_exists lib/"libtbbmalloc.a"
+
+    (testpath/"cores-types.cpp").write <<~EOS
+      #include <cstdlib>
+      #include <tbb/task_arena.h>
+
+      int main() {
+          const auto numa_nodes = tbb::info::numa_nodes();
+          const auto size = numa_nodes.size();
+          const auto type = numa_nodes.front();
+          return size != 1 || type != tbb::task_arena::automatic ? EXIT_SUCCESS : EXIT_FAILURE;
+      }
+    EOS
+
+    system ENV.cxx, "cores-types.cpp", "--std=c++14", "-DTBB_PREVIEW_TASK_ARENA_CONSTRAINTS_EXTENSION=1",
+                                      "-L#{lib}", "-ltbb", "-o", "core-types"
+    system "./core-types"
+
+    (testpath/"sum1-100.cpp").write <<~EOS
+      #include <iostream>
+      #include <tbb/blocked_range.h>
+      #include <tbb/parallel_reduce.h>
+
+      int main()
+      {
+        auto total = tbb::parallel_reduce(
+          tbb::blocked_range<int>(0, 100),
+          0.0,
+          [&](tbb::blocked_range<int> r, int running_total)
+          {
+            for (int i=r.begin(); i < r.end(); ++i) {
+              running_total += i + 1;
+            }
+
+            return running_total;
+          }, std::plus<int>()
+        );
+
+        std::cout << total << std::endl;
+        return 0;
+      }
+    EOS
+
+    system ENV.cxx, "sum1-100.cpp", "--std=c++14", "-L#{lib}", "-ltbb", "-o", "sum1-100"
+    assert_equal "5050", shell_output("./sum1-100").chomp
+
+    system python3, "-c", "import tbb"
+  end
+end
