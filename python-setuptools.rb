79ff0b44a786acf91514452d165392c96ca2d6df
Les Aker <me@lesaker.org>
2024-04-03 21:55:03 -0400

    add python-setuptools

---
+++ python-setuptools.rb
@@ -0,0 +1,27 @@
+class PythonSetuptools < Formula
+  desc "Easily download, build, install, upgrade, and uninstall Python packages"
+  homepage "https://setuptools.pypa.io/"
+  url "https://files.pythonhosted.org/packages/4d/5b/dc575711b6b8f2f866131a40d053e30e962e633b332acf7cd2c24843d83d/setuptools-69.2.0.tar.gz"
+  sha256 "0ff4183f8f42cd8fa3acea16c45205521a4ef28f73c6391d8a25e92893134f2e"
+  license "MIT"
+
+  depends_on "python@3.12" => [:build, :test]
+
+  def pythons
+    deps.map(&:to_formula)
+        .select { |f| f.name.match?(/^python@\d\.\d+$/) }
+        .map { |f| f.opt_libexec/"bin/python" }
+  end
+
+  def install
+    pythons.each do |python|
+      system python, "-m", "pip", "install", *std_pip_args, "."
+    end
+  end
+
+  test do
+    pythons.each do |python|
+      system python, "-c", "import setuptools"
+    end
+  end
+end
