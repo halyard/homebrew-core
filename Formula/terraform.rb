bae74b3f845ffdf81c5e0d36127a796f30e76b89
Les Aker <me@lesaker.org>
2024-05-04 13:26:44 -0400

    update terraform

--- Formula/terraform.rb
+++ Formula/terraform.rb
@@ -1,61 +1,43 @@
-class Terraform < Formula
-  desc "Tool to build, change, and version infrastructure"
-  homepage "https://www.terraform.io/"
-  # NOTE: Do not bump to v1.6.0+ as license changed to BUSL-1.1
-  # https://github.com/hashicorp/terraform/pull/33661
-  # https://github.com/hashicorp/terraform/pull/33697
-  url "https://github.com/hashicorp/terraform/archive/refs/tags/v1.5.7.tar.gz"
-  sha256 "6742fc87cba5e064455393cda12f0e0241c85a7cb2a3558d13289380bb5f26f5"
-  license "MPL-2.0"
-  head "https://github.com/hashicorp/terraform.git", branch: "main"
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
 
-  # TODO: Remove this if/when the formula is deprecated.
-  livecheck do
-    skip "Formula will not be updated due to BUSL license change"
+class Terraform < Formula
+  desc "Terraform"
+  homepage "https://www.terraform.io/"
+  version "1.8.2"
+
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://releases.hashicorp.com/terraform/1.8.2/terraform_1.8.2_darwin_amd64.zip"
+    sha256 "a71ada335aba64ac1851ffbb2cf8f727a06013d02474dd70c4571f585b1fe522"
   end
 
-  depends_on "go" => :build
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://releases.hashicorp.com/terraform/1.8.2/terraform_1.8.2_darwin_arm64.zip"
+    sha256 "f871f4c91eafec6e6e88253dc3cc0b6a21d63fa56fee5ee1629f3ce68a605873"
+  end
 
-  conflicts_with "tfenv", because: "tfenv symlinks terraform binaries"
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://releases.hashicorp.com/terraform/1.8.2/terraform_1.8.2_linux_amd64.zip"
+    sha256 "74f3cc4151e52d94e0ecbe900552adc9b8440b4a8dc12f7fdaab2d0280788acc"
+  end
 
-  # Needs libraries at runtime:
-  # /usr/lib/x86_64-linux-gnu/libstdc++.so.6: version `GLIBCXX_3.4.29' not found (required by node)
-  fails_with gcc: "5"
+  if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+    url "https://releases.hashicorp.com/terraform/1.8.2/terraform_1.8.2_linux_arm.zip"
+    sha256 "938c111ff0e0b895347c94e7865ff29edb088e205d715621d2fa63d509eeb163"
+  end
+
+  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    url "https://releases.hashicorp.com/terraform/1.8.2/terraform_1.8.2_linux_arm64.zip"
+    sha256 "e00726a0c8e4b06b31873446c639454358a0efb73a604198473f526d60e66100"
+  end
+
+  conflicts_with "terraform"
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w")
+    bin.install "terraform"
   end
 
   test do
-    minimal = testpath/"minimal.tf"
-    minimal.write <<~EOS
-      variable "aws_region" {
-        default = "us-west-2"
-      }
-
-      variable "aws_amis" {
-        default = {
-          eu-west-1 = "ami-b1cf19c6"
-          us-east-1 = "ami-de7ab6b6"
-          us-west-1 = "ami-3f75767a"
-          us-west-2 = "ami-21f78e11"
-        }
-      }
-
-      # Specify the provider and access details
-      provider "aws" {
-        access_key = "this_is_a_fake_access"
-        secret_key = "this_is_a_fake_secret"
-        region     = var.aws_region
-      }
-
-      resource "aws_instance" "web" {
-        instance_type = "m1.small"
-        ami           = var.aws_amis[var.aws_region]
-        count         = 4
-      }
-    EOS
-    system "#{bin}/terraform", "init"
-    system "#{bin}/terraform", "graph"
+    system "#{bin}/terraform --version"
   end
 end
