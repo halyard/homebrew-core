6242dbf26639eee064277e764fe56b70e3a64410
Les Aker <me@lesaker.org>
2024-05-04 21:34:06 -0400

    add flatbuffers

---
+++ Formula/flatbuffers.rb
@@ -0,0 +1,73 @@
+class Flatbuffers < Formula
+  desc "Serialization library for C++, supporting Java, C#, and Go"
+  homepage "https://google.github.io/flatbuffers"
+  url "https://github.com/google/flatbuffers/archive/refs/tags/v24.3.25.tar.gz"
+  sha256 "4157c5cacdb59737c5d627e47ac26b140e9ee28b1102f812b36068aab728c1ed"
+  license "Apache-2.0"
+  head "https://github.com/google/flatbuffers.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "cmake" => :build
+
+  conflicts_with "osrm-backend", because: "both install flatbuffers headers"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build",
+                    "-DFLATBUFFERS_BUILD_SHAREDLIB=ON",
+                    "-DFLATBUFFERS_BUILD_TESTS=OFF",
+                    *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    testfbs = <<~EOS
+      // example IDL file
+
+      namespace MyGame.Sample;
+
+      enum Color:byte { Red = 0, Green, Blue = 2 }
+
+      union Any { Monster }  // add more elements..
+
+        struct Vec3 {
+          x:float;
+          y:float;
+          z:float;
+        }
+
+        table Monster {
+          pos:Vec3;
+          mana:short = 150;
+          hp:short = 100;
+          name:string;
+          friendly:bool = false (deprecated);
+          inventory:[ubyte];
+          color:Color = Blue;
+        }
+
+      root_type Monster;
+
+    EOS
+    (testpath/"test.fbs").write(testfbs)
+
+    testjson = <<~EOS
+      {
+        pos: {
+          x: 1,
+          y: 2,
+          z: 3
+        },
+        hp: 80,
+        name: "MyMonster"
+      }
+    EOS
+    (testpath/"test.json").write(testjson)
+
+    system bin/"flatc", "-c", "-b", "test.fbs", "test.json"
+  end
+end
