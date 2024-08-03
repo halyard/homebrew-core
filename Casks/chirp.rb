85b52817289a939fb46478a79fe4a89d62a5cf7c
Les Aker <me@lesaker.org>
2024-08-03 11:43:14 -0400

    Update chirp.rb

--- Casks/chirp.rb
+++ Casks/chirp.rb
@@ -1,16 +1,19 @@
 cask "chirp" do
-  version "20230711"
-  sha256 "9e53ad22dd4bf0e0029c5249e3afceece9447be5192fea05ffa3370769268951"
+  version "20240706"
+  sha256 "62fa7e1e82a949b25487ee29e9a9980252d028dadd9dcd84127ac9a65af53cb1"
 
-  url "https://trac.chirp.danplanet.com/chirp_next/next-#{version}/chirp-next-#{version}.app.zip"
+  url "https://archive.chirpmyradio.com/chirp_next/next-#{version}/chirp-next-#{version}.app.zip",
+      verified: "archive.chirpmyradio.com/"
   name "CHIRP"
   desc "Tool for programming amateur radio"
   homepage "https://chirp.danplanet.com/projects/chirp/wiki/Home"
 
   livecheck do
-    url "https://trac.chirp.danplanet.com/chirp_next/"
+    url "https://archive.chirpmyradio.com/chirp_next/"
     regex(/next[._-]v?(\d+(?:\.\d+)*)/i)
   end
+
+  depends_on macos: ">= :big_sur"
 
   app "CHIRP.app"
 
