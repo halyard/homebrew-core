85330b64ecf1c0645ebf4b97d56958a8b70e097f
Les Aker <me@lesaker.org>
2024-08-03 11:49:02 -0400

    Update slack.rb

--- Casks/slack.rb
+++ Casks/slack.rb
@@ -1,19 +1,25 @@
 cask "slack" do
   arch arm: "arm64", intel: "x64"
 
-  on_mojave :or_older do
-    version "4.29.149"
-    sha256 arm:   "367f9d143dc7fc1cbe6813cb0f19cf9de357b8597eb4774b87125779804c3918",
-           intel: "13462738f9da5857b5ee00a9172c31bb9b87b35a4492d035b54bb842abca858c"
+  on_catalina do
+    version "4.33.90"
+    sha256 arm:   "8c060d33c7c451b58abaed380da4e6781089530d3b9c12da70e738e27c4eb47c",
+           intel: "7e0ba8a18a9cf95090ad80f58437d647eee5d1842ac4f15ea053c16c1629edde"
+
+    url "https://downloads.slack-edge.com/releases/macos/#{version}/prod/#{arch}/Slack-#{version}-macOS.dmg",
+        verified: "downloads.slack-edge.com/"
 
     livecheck do
       skip "Legacy version"
     end
   end
-  on_catalina :or_newer do
-    version "4.33.73"
-    sha256 arm:   "970c2afd38bd75c09590faa911c5d3e0e81766ed02d9ef1b018b2b5f82d64397",
-           intel: "6a86d0694903119b4e1cede375326feca555ec0c896d753b2802d53be8a41779"
+  on_big_sur :or_newer do
+    version "4.39.95"
+    sha256 arm:   "befe6facb7a4f1dcce041d5ecc0a9932b32aac88022c9c147a9631d36c72ab4e",
+           intel: "49c9ea0fc6626d6e7fb4edc40b4aafa3f38355e04efaf0e9dff1569207835f47"
+
+    url "https://downloads.slack-edge.com/desktop-releases/mac/#{arch}/#{version}/Slack-#{version}-macOS.dmg",
+        verified: "downloads.slack-edge.com/"
 
     livecheck do
       url "https://slack.com/ssb/download-osx"
@@ -21,15 +27,13 @@ cask "slack" do
     end
   end
 
-  url "https://downloads.slack-edge.com/releases/macos/#{version}/prod/#{arch}/Slack-#{version}-macOS.dmg",
-      verified: "downloads.slack-edge.com/releases/macos/"
   name "Slack"
   desc "Team communication and collaboration software"
   homepage "https://slack.com/"
 
   auto_updates true
-  conflicts_with cask: "homebrew/cask-versions/slack-beta"
-  depends_on macos: ">= :mojave"
+  conflicts_with cask: "slack@beta"
+  depends_on macos: ">= :catalina"
 
   app "Slack.app"
 
@@ -39,18 +43,15 @@ cask "slack" do
     "~/Library/Application Scripts/com.tinyspeck.slackmacgap",
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.tinyspeck.slackmacgap.sfl*",
     "~/Library/Application Support/Slack",
-    "~/Library/Caches/com.tinyspeck.slackmacgap",
-    "~/Library/Caches/com.tinyspeck.slackmacgap.ShipIt",
-    "~/Library/Containers/com.tinyspeck.slackmacgap",
-    "~/Library/Containers/com.tinyspeck.slackmacgap.SlackCallsService",
+    "~/Library/Caches/com.tinyspeck.slackmacgap*",
+    "~/Library/Containers/com.tinyspeck.slackmacgap*",
     "~/Library/Cookies/com.tinyspeck.slackmacgap.binarycookies",
     "~/Library/Group Containers/*.com.tinyspeck.slackmacgap",
     "~/Library/Group Containers/*.slack",
     "~/Library/HTTPStorages/com.tinyspeck.slackmacgap*",
     "~/Library/Logs/Slack",
     "~/Library/Preferences/ByHost/com.tinyspeck.slackmacgap.ShipIt.*.plist",
-    "~/Library/Preferences/com.tinyspeck.slackmacgap.helper.plist",
-    "~/Library/Preferences/com.tinyspeck.slackmacgap.plist",
+    "~/Library/Preferences/com.tinyspeck.slackmacgap*",
     "~/Library/Saved Application State/com.tinyspeck.slackmacgap.savedState",
     "~/Library/WebKit/com.tinyspeck.slackmacgap",
   ]
