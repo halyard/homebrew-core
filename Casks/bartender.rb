f8f04775533efe31c9c5a811b63ad2fcb632314a
Les Aker <me@lesaker.org>
2024-08-03 11:41:56 -0400

    Update bartender.rb

--- Casks/bartender.rb
+++ Casks/bartender.rb
@@ -1,16 +1,24 @@
 cask "bartender" do
-  version "4.2.21"
-  sha256 "28be16cbc69d1a2626c5a0e4846263c286d4e6cce95b68fb39b807ca9d360e8c"
+  on_ventura :or_older do
+    version "4.2.25"
+    sha256 "f1a2ecf300cf56aaf531324cba646442026cb0b63b4f90f46a1aee5b0804688a"
+  end
+  on_sonoma :or_newer do
+    version "5.0.54"
+    sha256 "302692a104c6714405cb7ec8c60dbe1eed226ca68d483074fa59df7d2d37da13"
+  end
 
-  url "https://macbartender.com/B2/updates/#{version.major}-#{version.minor}-#{version.patch.rjust(1, "0")}/Bartender%20#{version.major}.dmg",
-      referer: "https://www.macbartender.com"
+  url "https://macbartender.com/B2/updates/#{version.dots_to_hyphens}/Bartender%20#{version.major}.zip"
   name "Bartender"
-  desc "Menu bar icon organizer"
+  desc "Menu bar icon organiser"
   homepage "https://www.macbartender.com/"
 
   livecheck do
     url "https://www.macbartender.com/B2/updates/AppcastB#{version.major}.xml"
-    strategy :sparkle, &:short_version
+    regex(%r{https://macbartender.com/B2/updates/(\d+(?:-\d+)+)/Bartender%20#{version.major}.zip}i)
+    strategy :page_match do |page, regex|
+      page.scan(regex)&.map { |match| match[0].tr("-", ".") }
+    end
   end
 
   auto_updates true
@@ -18,14 +26,14 @@ cask "bartender" do
 
   app "Bartender #{version.major}.app"
 
-  uninstall delete:    [
+  uninstall launchctl: "com.surteesstudios.Bartender.BartenderInstallHelper",
+            quit:      "com.surteesstudios.Bartender",
+            delete:    [
               "/Library/Audio/Plug-Ins/HAL/BartenderAudioPlugIn.plugin",
               "/Library/PrivilegedHelperTools/com.surteesstudios.Bartender.BartenderInstallHelper",
               "/Library/ScriptingAdditions/BartenderHelper.osax",
               "/System/Library/ScriptingAdditions/BartenderSystemHelper.osax",
-            ],
-            launchctl: "com.surteesstudios.Bartender.BartenderInstallHelper",
-            quit:      "com.surteesstudios.Bartender"
+            ]
 
   zap trash: [
     "~/Library/Caches/com.surteesstudios.Bartender",
