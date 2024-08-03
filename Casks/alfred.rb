e3c0de5b18d0a5e0b41c311502c7518fc5fcd535
Les Aker <me@lesaker.org>
2024-08-03 11:41:17 -0400

    Update alfred.rb

--- Casks/alfred.rb
+++ Casks/alfred.rb
@@ -1,6 +1,6 @@
 cask "alfred" do
-  version "5.1.1,2138"
-  sha256 "8378a6c0cf93318033a5798ecdb31c95a22b5ac4cc1a195c99dc348df79a206b"
+  version "5.5,2257"
+  sha256 "eed7e21b531f3974ccd440f1cc56ba3ce6a2fed2d413864a9c4a4e6f2a637b54"
 
   url "https://cachefly.alfredapp.com/Alfred_#{version.csv.first}_#{version.csv.second}.dmg"
   name "Alfred"
@@ -20,15 +20,15 @@ cask "alfred" do
 
   app "Alfred #{version.major}.app"
 
-  uninstall login_item: "Alfred#{version.major}",
-            quit:       "com.runningwithcrayons.Alfred"
+  uninstall quit:       "com.runningwithcrayons.Alfred",
+            login_item: "Alfred#{version.major}"
 
   zap trash: [
     "~/Library/Application Support/Alfred",
     "~/Library/Caches/com.runningwithcrayons.Alfred",
     "~/Library/Cookies/com.runningwithcrayons.Alfred.binarycookies",
-    "~/Library/Preferences/com.runningwithcrayons.Alfred.plist",
     "~/Library/Preferences/com.runningwithcrayons.Alfred-Preferences.plist",
+    "~/Library/Preferences/com.runningwithcrayons.Alfred.plist",
     "~/Library/Saved Application State/com.runningwithcrayons.Alfred-Preferences.savedState",
   ]
 end
