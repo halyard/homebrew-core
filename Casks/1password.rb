a66ed280176e4a3d20fdcb57e68b70189ace0168
Les Aker <me@lesaker.org>
2024-08-03 11:40:50 -0400

    Update 1password.rb

--- Casks/1password.rb
+++ Casks/1password.rb
@@ -1,9 +1,9 @@
 cask "1password" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "8.10.8"
-  sha256 arm:   "f2555cebd45ad306319e555c7a1b4022e8eb9944179ac82c2bc013478be405ed",
-         intel: "1f6b78b046e6d8ca7cf5afbeafca054b2be0d7dcdcf5d02cab7a615ff8755558"
+  version "8.10.36"
+  sha256 arm:   "bf578287f70ea40e577266a602aade287450fb06a8050b6fbe634ee30bc5aba0",
+         intel: "bd886603d3758b33d1a371cf0e8b8ea632733302bb2e4087bb26310469083ca3"
 
   url "https://downloads.1password.com/mac/1Password-#{version}-#{arch}.zip"
   name "1Password"
@@ -16,16 +16,22 @@ cask "1password" do
   end
 
   auto_updates true
-  depends_on macos: ">= :high_sierra"
+  conflicts_with cask: [
+    "1password@beta",
+    "1password@nightly",
+  ]
+  depends_on macos: ">= :catalina"
 
   app "1Password.app"
 
   zap trash: [
     "~/Library/Application Scripts/2BUA8C4S2C.com.1password*",
+    "~/Library/Application Scripts/2BUA8C4S2C.com.agilebits",
     "~/Library/Application Scripts/com.1password.1password-launcher",
     "~/Library/Application Scripts/com.1password.browser-support",
     "~/Library/Application Support/1Password",
     "~/Library/Application Support/Arc/User Data/NativeMessagingHosts/com.1password.1password.json",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.1password.1password.sfl*",
     "~/Library/Application Support/CrashReporter/1Password*",
     "~/Library/Application Support/Google/Chrome Beta/NativeMessagingHosts/com.1password.1password.json",
     "~/Library/Application Support/Google/Chrome Canary/NativeMessagingHosts/com.1password.1password.json",
@@ -37,14 +43,14 @@ cask "1password" do
     "~/Library/Application Support/Microsoft Edge/NativeMessagingHosts/com.1password.1password.json",
     "~/Library/Application Support/Mozilla/NativeMessagingHosts/com.1password.1password.json",
     "~/Library/Application Support/Vivaldi/NativeMessagingHosts/com.1password.1password.json",
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.1password.1password.sfl2",
     "~/Library/Containers/2BUA8C4S2C.com.1password.browser-helper",
     "~/Library/Containers/com.1password.1password*",
+    "~/Library/Containers/com.1password.browser-support",
     "~/Library/Group Containers/2BUA8C4S2C.com.1password",
+    "~/Library/Group Containers/2BUA8C4S2C.com.agilebits",
+    "~/Library/Logs/1Password",
     "~/Library/Preferences/com.1password.1password.plist",
     "~/Library/Preferences/group.com.1password.plist",
     "~/Library/Saved Application State/com.1password.1password.savedState",
-    "~/Library/Application Scripts/2BUA8C4S2C.com.1password",
-    "~/Library/Application Scripts/2BUA8C4S2C.com.1password.browser-helper",
   ]
 end
