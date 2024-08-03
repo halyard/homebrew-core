16e63cf9726851a7a1f946383cf27417fc1200d6
Les Aker <me@lesaker.org>
2024-08-03 11:46:32 -0400

    Update iterm2.rb

--- Casks/iterm2.rb
+++ Casks/iterm2.rb
@@ -8,9 +8,17 @@ cask "iterm2" do
       skip "Legacy version"
     end
   end
-  on_mojave :or_newer do
-    version "3.4.19"
-    sha256 "e06563714ea0582f8a69653ef5024957483c174d7387b0e8d70369db98406832"
+  on_mojave do
+    version "3.4.16"
+    sha256 "b0941a008ead9f680e9f4937698b9b849acbb4e30ed1f3f100e3616cd6d49c0b"
+
+    livecheck do
+      skip "Legacy version"
+    end
+  end
+  on_catalina :or_newer do
+    version "3.5.3"
+    sha256 "5f789abff55ff2f69f6d51964c5fb7fd8599fcde113604b7a0e6ea39414ad4a9"
 
     livecheck do
       # workaround for
@@ -25,25 +33,26 @@ cask "iterm2" do
   url "https://iterm2.com/downloads/stable/iTerm2-#{version.dots_to_underscores}.zip"
   name "iTerm2"
   desc "Terminal emulator as alternative to Apple's Terminal app"
-  homepage "https://www.iterm2.com/"
+  homepage "https://iterm2.com/"
 
   auto_updates true
   conflicts_with cask: [
-    "homebrew/cask-versions/iterm2-beta",
-    "homebrew/cask-versions/iterm2-legacy",
-    "homebrew/cask-versions/iterm2-nightly",
+    "iterm2@beta",
+    "iterm2@nightly",
   ]
-  depends_on macos: ">= :catalina"
+  depends_on macos: ">= :sierra"
 
   app "iTerm.app"
 
   zap trash: [
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.googlecode.iterm2.itermai.sfl*",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.googlecode.iterm2.sfl*",
     "~/Library/Application Support/iTerm",
     "~/Library/Application Support/iTerm2",
-    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.googlecode.iterm2.sfl*",
     "~/Library/Caches/com.googlecode.iterm2",
+    "~/Library/Containers/iTermAI",
     "~/Library/Cookies/com.googlecode.iterm2.binarycookies",
     "~/Library/Preferences/com.googlecode.iterm2.plist",
-    "~/Library/Saved Application State/com.googlecode.iterm2.savedState",
+    "~/Library/Saved Application State/com.googlecode.iterm2*.savedState",
   ]
 end
