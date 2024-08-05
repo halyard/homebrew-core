8b3d21b3db39eb291f182c3a5a224018e83dfbcf
Les Aker <me@lesaker.org>
2024-08-05 17:46:56 -0400

    remove bottle

--- Formula/libgit2@1.7.rb
+++ Formula/libgit2@1.7.rb
@@ -10,16 +10,6 @@ class Libgit2AT17 < Formula
     regex(/^v?(1\.7(?:\.\d+)+)$/i)
   end
 
-  bottle do
-    sha256 cellar: :any,                 arm64_sonoma:   "0cad95e1f543ee0a6af1472c5a17e8f5bf09dafab3862ed08bd46e8f44cb0575"
-    sha256 cellar: :any,                 arm64_ventura:  "32585dd96ae9391b947813734680adef748792d6ead5ecdf3fbb8b9e495a069b"
-    sha256 cellar: :any,                 arm64_monterey: "444563550469e0d842b57e68bcd2d6344c914f065033736e0f96f6a61cd00156"
-    sha256 cellar: :any,                 sonoma:         "11fb0939977b4af263902257d496fe143276a489e2887c6f3c0382525a3bf281"
-    sha256 cellar: :any,                 ventura:        "00ce19e60bbe028b8771d15b9b29b1d8d249211fd2f5c6e053ad8af3b52842c4"
-    sha256 cellar: :any,                 monterey:       "d7c8dea4ca2f97faf4e37e618f469181d0c128208518b33fee1af1c60a86a301"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "8357f3e14b1bec47984f48b9f3e4d21a2386f1c98e42c163081e812f93706027"
-  end
-
   keg_only :versioned_formula
 
   depends_on "cmake" => :build
