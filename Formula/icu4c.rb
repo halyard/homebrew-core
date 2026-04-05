c18021e227f26b46f0603e19aeb82c5686273bf9
Les Aker <me@lesaker.org>
2026-04-04 21:04:44 -0400

    update icu4c

--- Formula/icu4c.rb
+++ Formula/icu4c.rb
@@ -1,4 +1,4 @@
-class Icu4cAT78 < Formula
+class Icu4c < Formula
   desc "C/C++ and Java libraries for Unicode and globalization"
   homepage "https://icu.unicode.org/home"
   url "https://github.com/unicode-org/icu/releases/download/release-78.3/icu4c-78.3-sources.tgz"
