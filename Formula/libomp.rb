1ef0de4045455c05802641d5286933f0410bf339
Les Aker <me@lesaker.org>
2026-04-04 21:05:31 -0400

    add libomp

---
+++ Formula/libomp.rb
@@ -0,0 +1,66 @@
+class Libomp < Formula
+  desc "LLVM's OpenMP runtime library"
+  homepage "https://openmp.llvm.org/"
+  url "https://github.com/llvm/llvm-project/releases/download/llvmorg-22.1.2/llvm-project-22.1.2.src.tar.xz"
+  sha256 "62f2f13ff25b1bb28ea507888e858212d19aafb65e8e72b4a65ee0629ec4ae0c"
+  license "MIT"
+  compatibility_version 1
+
+  livecheck do
+    url :stable
+    regex(/^llvmorg[._-]v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  # Ref: https://github.com/Homebrew/homebrew-core/issues/112107
+  keg_only "it can override GCC headers and result in broken builds"
+
+  depends_on "cmake" => :build
+  uses_from_macos "llvm" => :build
+
+  on_linux do
+    depends_on "python@3.14"
+  end
+
+  def install
+    # Disable LIBOMP_INSTALL_ALIASES, otherwise the library is installed as
+    # libgomp alias which can conflict with GCC's libgomp.
+    args = %w[
+      -DLIBOMP_INSTALL_ALIASES=OFF
+      -DLLVM_ENABLE_RUNTIMES=openmp
+      -DOPENMP_ENABLE_OMPT_TOOLS=OFF
+    ]
+    args << "-DOPENMP_ENABLE_LIBOMPTARGET=OFF" if OS.linux?
+
+    system "cmake", "-S", "runtimes", "-B", "build/shared", *args, *std_cmake_args
+    system "cmake", "--build", "build/shared"
+    system "cmake", "--install", "build/shared"
+
+    system "cmake", "-S", "runtimes", "-B", "build/static",
+                    "-DLIBOMP_ENABLE_SHARED=OFF",
+                    *args, *std_cmake_args
+    system "cmake", "--build", "build/static"
+    system "cmake", "--install", "build/static"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~CPP
+      #include <omp.h>
+      #include <array>
+      int main (int argc, char** argv) {
+        std::array<size_t,2> arr = {0,0};
+        #pragma omp parallel num_threads(2)
+        {
+            size_t tid = omp_get_thread_num();
+            arr.at(tid) = tid + 1;
+        }
+        if(arr.at(0) == 1 && arr.at(1) == 2)
+            return 0;
+        else
+            return 1;
+      }
+    CPP
+    system ENV.cxx, "-Werror", "-Xpreprocessor", "-fopenmp", "test.cpp", "-std=c++11",
+                    "-I#{include}", "-L#{lib}", "-lomp", "-o", "test"
+    system "./test"
+  end
+end
