f1db802303f64635060f8071466016dd53c00e12
Les Aker <me@lesaker.org>
2026-04-06 21:14:39 -0400

    fix php

--- Formula/php.rb
+++ Formula/php.rb
@@ -62,6 +62,7 @@ class Php < Formula
   depends_on "openldap"
   depends_on "openssl@3"
   depends_on "pcre2"
+  depends_on "rtmpdump"
   depends_on "sqlite"
   depends_on "tidy-html5"
   depends_on "unixodbc"
