49a7910b0fb64a62d22a60b67d0d3df8ec6d20ad
Les Aker <me@lesaker.org>
2026-07-10 21:20:59 -0400

    add mcp-grafana

---
+++ Formula/mcp-grafana.rb
@@ -0,0 +1,25 @@
+class McpGrafana < Formula
+  desc "MCP server for Grafana"
+  homepage "https://github.com/grafana/mcp-grafana"
+  url "https://github.com/grafana/mcp-grafana/archive/refs/tags/v0.17.1.tar.gz"
+  sha256 "bae7104e2ca4c4381b2dd753309b3a0ce4b2256df0fcf43e9982668e3f69c4b0"
+  license "Apache-2.0"
+  head "https://github.com/grafana/mcp-grafana.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X main.commit=#{tap.user} -X main.date=#{time.iso8601}"
+    system "go", "build", *std_go_args(ldflags:), "./cmd/mcp-grafana"
+  end
+
+  test do
+    json = <<~JSON
+      {"jsonrpc":"2.0","id":1,"method":"initialize","params":{"protocolVersion":"2025-03-26"}}
+      {"jsonrpc":"2.0","id":2,"method":"tools/list"}
+    JSON
+
+    output = pipe_output(bin/"mcp-grafana", json, 0)
+    assert_match "This server provides access to your Grafana instance and the surrounding ecosystem", output
+  end
+end
