89bb860036d8d9c78e7534e369f6dadabfafdf44
Les Aker <me@lesaker.org>
2026-07-12 14:25:15 -0400

    add tf lsp (#4)

---
+++ Formula/terraform-ls.rb
@@ -0,0 +1,41 @@
+class TerraformLs < Formula
+  desc "Terraform Language Server"
+  homepage "https://github.com/hashicorp/terraform-ls"
+  url "https://github.com/hashicorp/terraform-ls/archive/refs/tags/v0.38.8.tar.gz"
+  sha256 "1d2e725e49f08150ac4f9d3fab4961d56dde7f9f62922ce0ccdf93feb408c24c"
+  license "MPL-2.0"
+  head "https://github.com/hashicorp/terraform-ls.git", branch: "main"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.rawVersion=#{version}+#{tap.user}
+    ]
+    system "go", "build", *std_go_args(ldflags:)
+  end
+
+  test do
+    port = free_port
+    pid = spawn bin/"terraform-ls", "serve", "-port", port.to_s, File::NULL
+    begin
+      sleep 2
+      tcp_socket = TCPSocket.new("localhost", port)
+      tcp_socket.puts <<~EOF
+        Content-Length: 59
+
+        {"jsonrpc":"2.0","method":"initialize","params":{},"id":1}
+      EOF
+      assert_match "Content-Type", tcp_socket.gets("\n")
+    ensure
+      Process.kill("SIGINT", pid)
+      Process.wait(pid)
+    end
+  end
+end
