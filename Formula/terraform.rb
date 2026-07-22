498dee10f8af2ad9665da12d488ddfb3e8715431
Les Aker <me@lesaker.org>
2026-07-22 00:53:14 -0400

    update terraform

--- Formula/terraform.rb
+++ Formula/terraform.rb
@@ -1,64 +1,43 @@
+# Copyright IBM Corp. 2020, 2025
+# SPDX-License-Identifier: MPL-2.0
+
 class Terraform < Formula
-  desc "Tool to build, change, and version infrastructure"
+  desc "Terraform"
   homepage "https://www.terraform.io/"
-  # NOTE: Do not bump to v1.6.0+ as license changed to BUSL-1.1
-  # https://github.com/hashicorp/terraform/pull/33661
-  # https://github.com/hashicorp/terraform/pull/33697
-  url "https://github.com/hashicorp/terraform/archive/refs/tags/v1.5.7.tar.gz"
-  sha256 "6742fc87cba5e064455393cda12f0e0241c85a7cb2a3558d13289380bb5f26f5"
-  license "MPL-2.0"
-  head "https://github.com/hashicorp/terraform.git", branch: "main"
+  version "1.15.8"
 
-  # https://www.hashicorp.com/blog/hashicorp-adopts-business-source-license
-  deprecate! date: "2024-04-04", because: "changed its license to BUSL on the next release"
-  disable! date: "2025-04-12", because: "changed its license to BUSL on the next release"
-
-  depends_on "go" => :build
-
-  def install
-    system "go", "build", *std_go_args(ldflags: "-s -w")
+  if OS.mac? && Hardware::CPU.intel?
+    url "https://releases.hashicorp.com/terraform/1.15.8/terraform_1.15.8_darwin_amd64.zip"
+    sha256 "e2e812e783771159bf758fd4e55d6dc9bb08f63e2af2c63d212721807a02c5dc"
   end
 
-  def caveats
-    <<~EOS
-      We will not accept any new Terraform releases in homebrew/core (with the BUSL license).
-      The next release changed to a non-open-source license:
-      https://www.hashicorp.com/blog/hashicorp-adopts-business-source-license
-      See our documentation for acceptable licences:
-        https://docs.brew.sh/License-Guidelines
-    EOS
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://releases.hashicorp.com/terraform/1.15.8/terraform_1.15.8_darwin_arm64.zip"
+    sha256 "f210110c5698b94d803a7a63cdb0251b5455c150841478808e2bbb343f95ed68"
+  end
+
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://releases.hashicorp.com/terraform/1.15.8/terraform_1.15.8_linux_amd64.zip"
+    sha256 "d25ce7b6902013ad905db3d2eab0be4cd905887fe88b81a6171b8d5503c31f3d"
+  end
+
+  if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+    url "https://releases.hashicorp.com/terraform/1.15.8/terraform_1.15.8_linux_arm.zip"
+    sha256 "9e6964abf11a84db1fa84d67adbfb9caf52ef5d03fdc1beb122c66129b9337d4"
+  end
+
+  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+    url "https://releases.hashicorp.com/terraform/1.15.8/terraform_1.15.8_linux_arm64.zip"
+    sha256 "8891e9dcedc9e3b8950bc6af9d4d8af1f4cfade3062f53b9dc403a89f6ce8c9c"
+  end
+
+  conflicts_with "terraform"
+
+  def install
+    bin.install "terraform"
   end
 
   test do
-    minimal = testpath/"minimal.tf"
-    minimal.write <<~HCL
-      variable "aws_region" {
-        default = "us-west-2"
-      }
-
-      variable "aws_amis" {
-        default = {
-          eu-west-1 = "ami-b1cf19c6"
-          us-east-1 = "ami-de7ab6b6"
-          us-west-1 = "ami-3f75767a"
-          us-west-2 = "ami-21f78e11"
-        }
-      }
-
-      # Specify the provider and access details
-      provider "aws" {
-        access_key = "this_is_a_fake_access"
-        secret_key = "this_is_a_fake_secret"
-        region     = var.aws_region
-      }
-
-      resource "aws_instance" "web" {
-        instance_type = "m1.small"
-        ami           = var.aws_amis[var.aws_region]
-        count         = 4
-      }
-    HCL
-    system bin/"terraform", "init"
-    system bin/"terraform", "graph"
+    system "#{bin}/terraform --version"
   end
 end
